5b68ddfe9c79c4d8d4a79d2ce5ce4330786cda49
JUNGCHAEYOON <108243290+JUNGCHAEYOON@users.noreply.github.com>
2023-02-22 17:09:22 +0900

    툴바완성
    힘들다

--- app/src/main/java/com/example/risingcamp5week/main/MainActivity.kt
+++ app/src/main/java/com/example/risingcamp5week/main/MainActivity.kt
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.example.risingcamp5week.R
 import com.example.risingcamp5week.databinding.ActivityMainBinding
+import com.example.risingcamp5week.frag.*
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
@@ -21,5 +25,44 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         // activity_main.xml 이 root 로 설정됨
         setContentView(binding.root)
+
+        /* 바텀 네비게이션 */
+        val bnv_main = binding.bnvMain
+        bnv_main.setOnItemSelectedListener { item ->
+            changeFragment(
+                when(item.itemId){
+                    R.id.item1 ->{
+                        MatzipFragment()
+                        // Respond to navigation item 1 click
+                    }
+                    R.id.item2 ->{
+                        HalinFragment()
+                        // Respond to navigation item 1 click
+                    }
+                    R.id.item4 ->{
+                        SosikFragment()
+                        // Respond to navigation item 1 click
+                    }
+                    R.id.item5 ->{
+                        MypageFragment()
+                        // Respond to navigation item 1 click
+                    }
+                    else ->{
+                        AddFragment()
+                        // Respond to navigation item 1 click
+                    }
+
+                }
+            )
+            true
+        }
+        bnv_main.selectedItemId = R.id.item1
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fl_main, fragment)
+            .commit()
     }
 }
